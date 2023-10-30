83da7442e8826a60939297ce0600b68ee9f25429
yuri <yuri@example.com>
2023-10-30 12:36:31 +0900

    35. Exercise: Creating a Header Component

--- code/03 React & TypeScript Essentials/01 Starting Project/src/App.tsx
+++ code/03 React & TypeScript Essentials/01 Starting Project/src/App.tsx
@@ -1,9 +1,16 @@
 import CourseGoal from "./components/CourseGoal";
+import GoalImage from "./assets/goals.jpg";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <main>
-      <CourseGoal title="タイトル"><p>説明書き</p></CourseGoal>
+      <Header image={{ src: GoalImage, alt: "ゴールのリスト" }}>
+        <h1>コースのゴール</h1>
+      </Header>
+      <CourseGoal title="タイトル">
+        <p>説明書き</p>
+      </CourseGoal>
     </main>
   );
 }
---
+++ code/03 React & TypeScript Essentials/01 Starting Project/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { type ReactNode } from "react";
+
+type HeaderProps = {
+  image: {
+    src: string;
+    alt: string;
+  };
+  children: ReactNode;
+};
+
+const Header = ({ image, children }: HeaderProps) => {
+  return (
+    <header>
+      <img {...image} />
+      {children}
+    </header>
+  );
+};
+
+export default Header;
