919b87f7c2d463fd678095aa15646e674c63b8e2
yuri <yuri@example.com>
2023-10-30 12:45:57 +0900

    36. Using useState() and TypeScript

--- code/03 React & TypeScript Essentials/01 Starting Project/src/App.tsx
+++ code/03 React & TypeScript Essentials/01 Starting Project/src/App.tsx
@@ -2,12 +2,24 @@ import CourseGoal from "./components/CourseGoal";
 import GoalImage from "./assets/goals.jpg";
 import Header from "./components/Header";
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  const handleAddGoal = () => {
+    setGoals();
+  };
+
   return (
     <main>
       <Header image={{ src: GoalImage, alt: "ゴールのリスト" }}>
         <h1>コースのゴール</h1>
       </Header>
+      <button onClick={handleAddGoal}>ゴールを追加</button>
       <CourseGoal title="タイトル">
         <p>説明書き</p>
       </CourseGoal>
